a99d14deb287b9118dc420f0a4da26a6b96c972d
rlenafk <arlenabtibol21@gmail.com>
2026-05-28 15:26:34 -0300

    adicionado confirmar chegada do locatário na aba gerente

--- src/config/schema.sql
+++ src/config/schema.sql
@@ -1,7 +1,6 @@
 CREATE DATABASE IF NOT EXISTS jogafacil CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 USE jogafacil;
 
-
 CREATE TABLE IF NOT EXISTS usuarios (
     id         INT AUTO_INCREMENT PRIMARY KEY,
     nome       VARCHAR(100)  NOT NULL,
